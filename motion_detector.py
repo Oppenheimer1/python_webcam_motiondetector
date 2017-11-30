f3c7c24224648474ae4eac679128547328fee169
LARS BERG <larsgbr@gmail.com>
2017-11-30 13:32:30 -0600

    Created the functionality to be able to detect objects from the webcam

--- motion_detector.py
+++ motion_detector.py
@@ -14,13 +14,27 @@ while True:
 		first_frame=gray
 		continue
 
-	delta_frame=cv2.absdiff(first_frame,gray)
+	delta_frame = cv2.absdiff(first_frame,gray)
+	thresh_frame = cv2.threshold(delta_frame,30,255, cv2.THRESH_BINARY)[1]
+	thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+	(_,cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	for contour in cnts:
+		if cv2.contourArea(contour) < 1000:
+			continue
+		(x, y, w, h)=cv2.boundingRect(contour)
+		cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
 
 	cv2.imshow("Gray Frame",gray)
 	cv2.imshow("Delta Frame",delta_frame)
+	cv2.imshow("threshold Frame", thresh_frame)
+	cv2.imshow("Color Frame",frame)
+
 
 	key=cv2.waitKey(1)
 	print(gray)
+	print(delta_frame)
 
 	if key==ord('q'):
 		break
